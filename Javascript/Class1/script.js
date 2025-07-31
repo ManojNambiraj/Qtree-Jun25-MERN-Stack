4b14b0d7bcac79a61f4948fdcd068dd72b3bf619
ManojNambiraj <postboxmano@gmail.com>
2025-07-31 20:19:05 +0530

    JS intro

---
+++ Javascript/Class1/script.js
@@ -0,0 +1,31 @@
+// Javascript
+
+// Versions:
+
+    // ES5 ---> old version --> 2015
+    // ES6 ---> New version
+
+// Hoisting (or) Variable Declaration
+
+    // var --> ES5
+
+        // var a = 1200
+
+        // var a = 500
+
+        // console.log(a);
+
+    // let --> ES6
+
+        // let a = 1200
+
+        // a = 1500
+
+        // console.log(a);
+
+
+    // const
+
+        const a = 500
+
+        console.log(a);
