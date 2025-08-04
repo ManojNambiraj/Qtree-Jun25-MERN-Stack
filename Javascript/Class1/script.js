9526dfb89d557b78f59272c635c110e6296471e5
ManojNambiraj <postboxmano@gmail.com>
2025-08-04 20:05:17 +0530

    JS Intro

--- Javascript/Class1/script.js
+++ Javascript/Class1/script.js
@@ -7,25 +7,49 @@
 
 // Hoisting (or) Variable Declaration
 
-    // var --> ES5
+    // var
 
-        // var a = 1200
+        // var a = 10
 
-        // var a = 500
+        // var a = 300
+
+        // console.log(a);
+        
+    // let
+
+        // let a
 
         // console.log(a);
 
-    // let --> ES6
-
-        // let a = 1200
-
-        // a = 1500
+        // a = 30
 
         // console.log(a);
-
-
+    
     // const
 
-        const a = 500
+        // const a = 10
 
-        console.log(a);
+        // console.log(a);
+
+// Scoping:
+
+    // Global scope
+
+        // {
+        //     var a = 10
+
+        //     console.log("inside: ", a); 
+        // }
+
+        // console.log("outside: ", a);
+
+    // Local scope
+
+        // {
+        //   const a = 10;
+
+        //   console.log("inside: ", a);
+        // }
+
+        // console.log("outside: ", a);
+        
