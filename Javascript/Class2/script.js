b0822b838bd36a23900032a7c32533a486abc879
ManojNambiraj <postboxmano@gmail.com>
2025-08-05 20:03:19 +0530

    JS Operators

---
+++ Javascript/Class2/script.js
@@ -0,0 +1,71 @@
+// Variables:
+
+    // let myAgeInMySchoolDays = 20
+    // let my_age_in_my_school_Days = 20
+
+    // console.log(my_age_in_my_school_Days);
+
+// Dataypes:
+
+    // let temp = null
+
+    // console.log(temp);
+    // console.log(typeof(temp));
+
+// Operators:
+
+    // Arithmetic Operators
+
+        // (+, -, *, /, %, **, ++, --)
+
+            // let a = 5
+            // let b = 3
+            // let result
+
+            // result = a + b
+            // result = a - b
+            // result = a * b
+            // result = a / b
+            // result = a % b
+            // result = a ** b
+            // result = ++a
+            // result = --a
+
+            // console.log(result);
+        
+    // Assignment Operators
+
+        // (=, +=, -=, *=, /=, %=, **=)
+
+            // let a = 7
+
+            // a += 3   // a = a + 3
+            // a -= 3   // a = a - 3
+            // a *= 3   // a = a * 3
+            // a /= 3   // a = a / 3
+            // a %= 3   // a = a % 3
+            // a **= 3   // a = a ** 3
+
+            // console.log(a);
+        
+    // Comparison Operators ---> boolean
+        // (==, ===, !=, !==, >, >=, <, <=)
+
+            // let age = 18
+            // let result
+
+            // result = (age == 18)
+            // result = (age === 18)
+            // result = (age != 18)
+            // result = (age !== 18)
+            // result = (age > 18)
+            // result = (age >= 18)
+            // result = (age < 18)
+            // result = (age <= 18)
+
+            // console.log(result);
+        
+
+    // Logical Operators
+    // Ternary Operators
+    // Type Operators
