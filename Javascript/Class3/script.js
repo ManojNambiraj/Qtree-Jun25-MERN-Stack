a40462753562621c5886d495e88cac953804da8b
ManojNambiraj <postboxmano@gmail.com>
2025-08-11 20:10:36 +0530

    JS Arrays and Objects

---
+++ Javascript/Class3/script.js
@@ -0,0 +1,84 @@
+// Array:
+
+// Example 1:
+    // let a = [10, 20, 30, 40, 50, "hello", true]
+    // //       #0  #1  #2  #3  #4..............  
+
+    // console.log(a);
+    // console.log(a[6]);
+
+// Example 2:
+    // let fruits = ["apple", "banana", "orange", "grapes"]
+    // let nums = [100, 200, 300];
+
+    // // console.log(fruits);
+
+    // // fruits.pop()
+    // // fruits.push("kiwi")
+    // let temp = fruits.concat(nums)
+
+    // console.log(temp);
+
+// Rest (or) Spread Operator using array
+
+    // let fruits = ["apple", "banana", "orange"];
+    // let a = [10, 20, 30, 40]
+
+    // fruits = [...fruits, ...a]
+
+    // console.log(fruits);
+
+// Objects  -->  {key: value}
+
+    // let studentDetails = {
+    //     name: "Raj", 
+    //     age: 23, 
+    //     dept: "IT", 
+    //     mobile: 987654321,
+    //     favColor: ["red", "black", "pink"],
+    //     address: {
+    //         dNo: 2,
+    //         street: "Gandhi street",
+    //         landmark: ["watertank", "kovil"]
+    //     }
+    // }
+
+    // console.log(studentDetails);
+    // console.log(studentDetails.mobile);
+    // console.log(studentDetails.dept);
+    // console.log(studentDetails.favColor[1]);
+    // console.log(studentDetails.address.street);
+
+// Destructuring:
+
+    // Array Destructuring
+
+        // let fruits = ["apple", "banana", "orange"];
+
+        // let [red, yellow, oran] = fruits
+
+        // console.log(red);
+        // console.log(oran);
+
+    // Object Destructuring
+
+        // let studentDetails = {
+        //   name: "Raj",
+        //   age: 23,
+        //   dept: "IT",
+        //   mobile: 987654321,
+        //   favColor: ["red", "black", "pink"],
+        //   address: {
+        //     dNo: 2,
+        //     street: "Gandhi street",
+        //     landmark: ["watertank", "kovil"],
+        //   },
+        // };
+
+        // const {name, age, dept} = studentDetails
+
+        // console.log(name);
+        // console.log(age);
+        // console.log(dept);
+    
+    
