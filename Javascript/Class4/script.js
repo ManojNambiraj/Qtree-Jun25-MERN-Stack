3d8a3cc9f233e356c9efd73b673a7883d353dbf6
ManojNambiraj <postboxmano@gmail.com>
2025-08-12 20:17:55 +0530

    JS Functions

---
+++ Javascript/Class4/script.js
@@ -0,0 +1,53 @@
+// Functions:
+
+    // 1. Built-in functions
+    // 2. User-defined functions
+
+    // Syntax
+
+        // function demo(){      // Function definition
+        //     console.log("Hello, Good evening");
+        // }
+
+        // demo()               // Function call
+
+    // 1. Normal Function:
+
+        // function multiplication(a, b){
+            // return a * b 
+        // }
+
+        // const result = multiplication(100, 5)
+
+        // console.log(result);
+
+    // 2. Ananymous function
+
+        // const demo = function(n){
+        //     return n * 2
+        // }
+
+        // console.log(demo(100));
+
+    // 3. Arrow funcion
+
+        // const arr = (a, b) => a * b;
+
+        // var result = arr(5, 2);
+
+        // console.log(result);
+
+    // 4. IIFE Function
+        //   (Immediatly Invoked Function Expression)
+
+        //   (() => {
+        //     console.log("I'm IIFE");
+        //   })()
+
+    let obj = {
+        name: "Raj",
+        age: 22,
+        BMICal: function(a, b){
+            a * b
+        }
+    }
