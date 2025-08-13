e5e6138fb0f6f4177933d10857b8013238c762de
ManojNambiraj <postboxmano@gmail.com>
2025-08-13 20:18:06 +0530

    js callback

---
+++ Javascript/Class5/script.js
@@ -0,0 +1,46 @@
+// Callback functions:
+
+    // function display(result){
+    //     console.log(result);
+    // }
+
+    // function demo(a, b, display){
+    //     let ans = a * b
+
+    //     display(ans);
+    // }
+
+    // demo(10, 5, display)
+
+    // function demo(){
+    //     console.log("hello");
+    // }
+
+    // setTimeout(function(){
+    //     console.log("hello");
+    // }, 5000)
+
+    // setInterval(() => {
+    //     console.log("hi");
+    // }, 4000)
+
+// Array
+
+// let fruits = ["apple", "banana", "orange", "grapes", "kiwi"]
+
+    // for(let i = 0; i < fruits.length; i++){
+    //     console.log(fruits[i]);
+    // }
+
+// Array methods ---> MRF (map, reduce, filter)
+
+    // Map:
+
+        // fruits.map((a) => console.log(a))
+
+    let nums = [20, 42, 77, 98, 1, 7, 9, 54]
+
+    const result = nums.map((item) => item * 2);
+
+    console.log(result);
+    
