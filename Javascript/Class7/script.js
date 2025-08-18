fd9297a6827cafb40dac66dc69dc98ef43780b03
ManojNambiraj <postboxmano@gmail.com>
2025-08-18 20:10:41 +0530

    JS DOM Intro

---
+++ Javascript/Class7/script.js
@@ -0,0 +1,19 @@
+// DOM ---> Document Object Model
+
+let ele1 = document.getElementById("demo")
+
+console.log(ele1);
+
+
+ele1.innerText = "Good evening"
+    // // ele.innerHTML = "<a href='#'>Hello</a>"
+
+    // ele.style.color = "red"
+
+let ele = document.getElementsByTagName("p")
+
+console.log(ele);
+
+
+ele[0].innerText = "hello"
+
